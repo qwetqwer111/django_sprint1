f1dcfd844b7ed2441b9b17fce7fcd024390e8046
qwetqwer111 <158411752+qwetqwer111@users.noreply.github.com>
2024-04-17 21:52:58 +1100

    Add files via upload

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+app_name = 'pages'
+
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
